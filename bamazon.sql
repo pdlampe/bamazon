013bf964bd34f78d27690c42796852ab34dffed8
Paul David Lampe <pdlampe@gmail.com>
2019-08-19 22:38:00 -0500

    Content in table

--- bamazon.sql
+++ bamazon.sql
@@ -11,3 +11,15 @@ CREATE TABLE products (
     stock_quantity INT NOT NULL,
     PRIMARY KEY (item_id)
 );
+
+INSERT INTO products (product_name, department_name, price, stock_quantity) VALUES
+('Town and Country Plates', 'Kitchen', 24.99, 6),
+('Dry Erase Board', 'Office Products', 89.99, 4),
+('iPad', 'Electronics', 499.99, 30),
+('Nike Running Shoes', 'Apparel', 129.95, 18),
+('Bluetooth Wireless Headphones', 'Electronics', 49.99, 5),
+('Picture Frame', 'Home Essentials', 19.99, 10),
+('Town and Country Drinking Glasses', 'Kitchen', 9.99, 20),
+('Gold Rimmed Glass Tealight Candleholders Set of 3', 'Home Essentials', 17.99, 10),
+('JBL Bluetooth Speaker', 'Electronics', 99.99, 5),
+('SOHO High-Back Executive Chair', 'Office Products', 359.99, 60);
