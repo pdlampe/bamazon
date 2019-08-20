dbb5f36950cda7a816d3198c73720541fb02a963
Paul David Lampe <pdlampe@gmail.com>
2019-08-19 22:53:40 -0500

    Customer JavaScript file created

---
+++ bamazonCustomer.js
@@ -0,0 +1,14 @@
+var inquirer = require('inquirer');
+var mysql = require('mysql');
+
+var connection = mysql.createConnection({
+    host: 'localhost',
+    port: 3306,
+
+    // Your username
+    user: 'root',
+
+    // Your password
+    password: '',
+    database: 'bamazon'
+});
