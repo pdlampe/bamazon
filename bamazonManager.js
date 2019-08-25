7ff53f6a66e766bf75ebc03dcbc0c8255c66fff0
Paul David Lampe <pdlampe@gmail.com>
2019-08-24 19:21:43 -0500

    If Else code to establish selection options

--- bamazonManager.js
+++ bamazonManager.js
@@ -12,3 +12,41 @@ var connection = mysql.createConnection({
     password: 'SourPablo1985!!',
     database: 'bamazon'
 });
+
+function promptManager() {
+    inquirer.prompt([
+        {
+            type: 'list',
+            name: 'option',
+            message: 'Please select an option:',
+            choices: ['View products for sale', 'View low inventory', 'Add to Inventory', 'Add New Product'],
+            filer: function (selection) {
+                if (selection === 'View products for sale') {
+                    return 'sale';
+                } else if (val === 'View low inventory') {
+                    return 'lowInventory';
+                } else if (val === 'Add to inventory') {
+                    return 'addInventory';
+                } else if (val === 'Add new product') {
+                    return 'newProduct';
+                } else {
+                    console.log('ERROR: Unsupported operation!');
+                    exit(1);
+                }
+            }
+
+        }
+    ])
+
+
+
+}
+
+
+
+function runManagerAPP() {
+
+    promptManager();
+}
+
+runManagerAPP();
